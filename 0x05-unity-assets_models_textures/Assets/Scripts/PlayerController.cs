a8905c87126bc76a54551c761c129819c3e6cd35
dilanrinconhbtn <937@holbertonschool.com>
2020-06-07 17:52:18 -0500

    setting camera

---
+++ 0x05-unity-assets_models_textures/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalMove;
+    public float verticalMove;
+    private Vector3 playerInput;
+    public CharacterController player;
+    public float gravity = 9.8f;
+
+    public float playerSpeed = 0.5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxis("Horizontal");
+        verticalMove = Input.GetAxis("Vertical");
+
+        playerInput = new Vector3(horizontalMove, 0, verticalMove);
+        playerInput = Vector3.ClampMagnitude(playerInput, 1);
+
+        player.Move(playerInput * playerSpeed * Time.deltaTime);
+
+    }
+    private void FixedUpdate() {
+        
+    }
+
+    void SetGravity(){
+        movePlayer.y = -gravity * Time.deltaTime;
+    }
+}
